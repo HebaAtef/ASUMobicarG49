5306030bded27ec48c5b2a11a3337c025f6a4371
MernaElhassan <36541082+MernaElhassan@users.noreply.github.com>
2018-03-12 05:57:02 -0700

    Update Phase1Code.ino

--- ProjectFiles/Phase1Code.ino
+++ ProjectFiles/Phase1Code.ino
@@ -134,47 +134,6 @@ void moveBackward(){
   
 }
 
-void turnRight(){
-
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(EN1, obsp);
-  analogWrite(EN2, obsp);
-  
-  delay(500);
-  
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(EN1, obsp);
-  analogWrite(EN2, obsp);
- 
-  
-  
-}
-
-void turnLeft(){
-
- digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  analogWrite(EN1, obsp);
-  analogWrite(EN2, obsp);
-
-  delay(500);
-  
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(EN1, obsp);
-  analogWrite(EN2, obsp);
-}
-
 void setup() {
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
