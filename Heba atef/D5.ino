f34922dd208621296aab920f1ab4d9821743d413
HebaAtef <36536598+HebaAtef@users.noreply.github.com>
2018-04-11 21:20:05 +0200

    Add files via upload

---
+++ Heba atef/D5.ino
@@ -0,0 +1,13 @@
+#define LED 13
+#define BUTTON 7
+void setup() {
+pinMode(LED,OUTPUT);
+pinMode(BUTTON,INPUT);
+digitalWrite(BUTTON , HIGH);
+}
+
+void loop() {
+  int value = digitalRead(BUTTON);
+  digitalWrite(LED,value);
+  
+}
