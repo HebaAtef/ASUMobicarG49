88762f04e6bf9a9326461a21e12cc630e260ccc9
Hebahamdy97 <36519795+Hebahamdy97@users.noreply.github.com>
2018-04-13 15:14:23 +0200

    Rename sketch_mar01a.ino to uploaded_Examples/sketch_mar01a.ino

---
+++ uploaded_Examples/sketch_mar01a.ino
@@ -0,0 +1,16 @@
+# include<LiquidCrystal.h>
+ LiquidCrystal lcd(8,9,4,5,6,7);
+float temp;
+int lm=A0;
+void setup() {
+  // put your setup code here, to run once:
+lcd.begin(16,2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+temp=analogRead(lm);
+temp=(5*temp*100)/1024;
+lcd.setCursor(6,0);
+lcd.print(temp);
+}
