36af13f307ace36c43bdd9515be4a3bb7184c333
Hebahamdy97 <36519795+Hebahamdy97@users.noreply.github.com>
2018-04-13 15:16:21 +0200

    Rename file.ino to uploaded_Examples/file.ino

---
+++ uploaded_Examples/file.ino
@@ -0,0 +1,12 @@
+void setup()
+{
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(7, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(1500); // Wait for 1000 millisecond(s)
+}
